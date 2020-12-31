466110206e461105aa5f6a542fd613f34c420300
Eris <erisapps@outlook.be>
2020-12-31 22:33:00 +0100

    Split up and got rid of most of the warnings in Tween-related classes/interfaces

---
+++ ChartPlugin/Utilities/ITween.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace DigitalRuby.Tween
+{
+	/// <summary>
+	/// Interface for a tween object.
+	/// </summary>
+	public interface ITween
+	{
+		/// <summary>
+		/// The key that identifies this tween - can be null
+		/// </summary>
+		object Key { get; }
+
+		/// <summary>
+		/// Gets the current state of the tween.
+		/// </summary>
+		TweenState State { get; }
+
+		/// <summary>
+		/// Start the tween.
+		/// </summary>
+		void Start();
+
+		/// <summary>
+		/// Pauses the tween.
+		/// </summary>
+		void Pause();
+
+		/// <summary>
+		/// Resumes the paused tween.
+		/// </summary>
+		void Resume();
+
+		/// <summary>
+		/// Stops the tween.
+		/// </summary>
+		/// <param name="stopBehavior">The behavior to use to handle the stop.</param>
+		void Stop(TweenStopBehavior stopBehavior);
+
+		/// <summary>
+		/// Updates the tween.
+		/// </summary>
+		/// <param name="elapsedTime">The elapsed time to add to the tween.</param>
+		/// <returns>True if done, false if not</returns>
+		bool Update(float elapsedTime);
+	}
+
+	/// <summary>
+	/// Interface for a tween object that handles a specific type.
+	/// </summary>
+	/// <typeparam name="T">The type to tween.</typeparam>
+	public interface ITween<T> : ITween where T : struct
+	{
+		/// <summary>
+		/// Gets the current value of the tween.
+		/// </summary>
+		T CurrentValue { get; }
+
+		/// <summary>
+		/// Gets the current progress of the tween.
+		/// </summary>
+		float CurrentProgress { get; }
+
+		/// <summary>
+		/// Initialize a tween.
+		/// </summary>
+		/// <param name="start">The start value.</param>
+		/// <param name="end">The end value.</param>
+		/// <param name="duration">The duration of the tween.</param>
+		/// <param name="scaleFunc">A function used to scale progress over time.</param>
+		/// <param name="progress">Progress callback</param>
+		/// <param name="completion">Called when the tween completes</param>
+		Tween<T> Setup(T start, T end, float duration, Func<float, float> scaleFunc, Action<ITween<T>> progress, Action<ITween<T>>? completion = null);
+	}
+}
---
+++ ChartPlugin/Utilities/TweenScaleFunctions.cs
@@ -0,0 +1,170 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace DigitalRuby.Tween
+{
+	/// <summary>
+	/// Tween scale functions
+	/// Implementations based on http://theinstructionlimit.com/flash-style-tweeneasing-functions-in-c, which are based on http://www.robertpenner.com/easing/
+	/// </summary>
+	public static class TweenScaleFunctions
+	{
+		private const float HALF_PI = Mathf.PI * 0.5f;
+
+		/// <summary>
+		/// A linear progress scale function.
+		/// </summary>
+		public static float Linear(float progress)
+		{
+			return progress;
+		}
+
+		/// <summary>
+		/// A quadratic (x^2) progress scale function that eases in.
+		/// </summary>
+		public static float QuadraticEaseIn(float progress)
+		{
+			return EaseInPower(progress, 2);
+		}
+
+		/// <summary>
+		/// A quadratic (x^2) progress scale function that eases out.
+		/// </summary>
+		public static float QuadraticEaseOut(float progress)
+		{
+			return EaseOutPower(progress, 2);
+		}
+
+		/// <summary>
+		/// A quadratic (x^2) progress scale function that eases in and out.
+		/// </summary>
+		public static float QuadraticEaseInOut(float progress)
+		{
+			return EaseInOutPower(progress, 2);
+		}
+
+		/// <summary>
+		/// A cubic (x^3) progress scale function that eases in.
+		/// </summary>
+		public static float CubicEaseIn(float progress)
+		{
+			return EaseInPower(progress, 3);
+		}
+
+		/// <summary>
+		/// A cubic (x^3) progress scale function that eases out.
+		/// </summary>
+		[SuppressMessage("ReSharper", "UnusedMember.Local")]
+		private static float CubicEaseOut(float progress)
+		{
+			return EaseOutPower(progress, 3);
+		}
+
+		/// <summary>
+		/// A cubic (x^3) progress scale function that eases in and out.
+		/// </summary>
+		public static float CubicEaseInOut(float progress)
+		{
+			return EaseInOutPower(progress, 3);
+		}
+
+		/// <summary>
+		/// A quartic (x^4) progress scale function that eases in.
+		/// </summary>
+		[SuppressMessage("ReSharper", "UnusedMember.Local")]
+		private static float QuarticEaseIn(float progress)
+		{
+			return EaseInPower(progress, 4);
+		}
+
+		/// <summary>
+		/// A quartic (x^4) progress scale function that eases out.
+		/// </summary>
+		[SuppressMessage("ReSharper", "UnusedMember.Local")]
+		public static float QuarticEaseOut(float progress)
+		{
+			return EaseOutPower(progress, 4);
+		}
+
+		/// <summary>
+		/// A quartic (x^4) progress scale function that eases in and out.
+		/// </summary>
+		[SuppressMessage("ReSharper", "UnusedMember.Local")]
+		public static float QuarticEaseInOut(float progress)
+		{
+			return EaseInOutPower(progress, 4);
+		}
+
+		/// <summary>
+		/// A quintic (x^5) progress scale function that eases in.
+		/// </summary>
+		public static float QuinticEaseIn(float progress)
+		{
+			return EaseInPower(progress, 5);
+		}
+
+		/// <summary>
+		/// A quintic (x^5) progress scale function that eases out.
+		/// </summary>
+		public static float QuinticEaseOut(float progress)
+		{
+			return EaseOutPower(progress, 5);
+		}
+
+		/// <summary>
+		/// A quintic (x^5) progress scale function that eases in and out.
+		/// </summary>
+		public static float QuinticEaseInOut(float progress)
+		{
+			return EaseInOutPower(progress, 5);
+		}
+
+		/// <summary>
+		/// A sine progress scale function that eases in.
+		/// </summary>
+		public static float SineEaseIn(float progress)
+		{
+			return Mathf.Sin(progress * HALF_PI - HALF_PI) + 1;
+		}
+
+		/// <summary>
+		/// A sine progress scale function that eases out.
+		/// </summary>
+		public static float SineEaseOut(float progress)
+		{
+			return Mathf.Sin(progress * HALF_PI);
+		}
+
+		/// <summary>
+		/// A sine progress scale function that eases in and out.
+		/// </summary>
+		public static float SineEaseInOut(float progress)
+		{
+			return (Mathf.Sin(progress * Mathf.PI - HALF_PI) + 1) / 2;
+		}
+
+		private static float EaseInPower(float progress, int power)
+		{
+			return Mathf.Pow(progress, power);
+		}
+
+		private static float EaseOutPower(float progress, int power)
+		{
+			var sign = power % 2 == 0 ? -1 : 1;
+			return (sign * (Mathf.Pow(progress - 1, power) + sign));
+		}
+
+		private static float EaseInOutPower(float progress, int power)
+		{
+			progress *= 2.0f;
+			if (progress < 1)
+			{
+				return Mathf.Pow(progress, power) / 2.0f;
+			}
+
+			var sign = power % 2 == 0 ? -1 : 1;
+			return (sign / 2.0f * (Mathf.Pow(progress - 2, power) + sign * 2));
+		}
+	}
+}
