65414d33aecab4c8835113e82bac2fdc2fd8c170
Eris <erisapps@outlook.be>
2021-01-12 22:54:11 +0100

    Fixed chart still showing despite being disabled in menu

--- ChartPlugin/Installers/SvcGameInstaller.cs
+++ ChartPlugin/Installers/SvcGameInstaller.cs
@@ -6,8 +6,22 @@ namespace SongChartVisualizer.Installers
 {
 	internal class SvcGameInstaller : Installer<SvcGameInstaller>
 	{
+		private readonly PluginConfig _pluginConfig;
+		private readonly GameplayCoreSceneSetupData _gameCoreSceneSetupData;
+
+		public SvcGameInstaller(PluginConfig pluginConfig, GameplayCoreSceneSetupData gameplayCoreSceneSetupData)
+		{
+			_pluginConfig = pluginConfig;
+			_gameCoreSceneSetupData = gameplayCoreSceneSetupData;
+		}
+
 		public override void InstallBindings()
 		{
+			if (!_pluginConfig.EnablePlugin || _gameCoreSceneSetupData.playerSpecificSettings.noTextsAndHuds)
+			{
+				return;
+			}
+
 			Container.BindInterfacesAndSelfTo<ChartViewController>().FromNewComponentAsViewController().AsSingle();
 		}
 	}
