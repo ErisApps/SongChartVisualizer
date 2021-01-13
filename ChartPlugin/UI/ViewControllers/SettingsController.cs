0fafd8e2a31f5b0b4139f7072e5f7c4c8a73bb8a
Eris <erisapps@outlook.be>
2021-01-13 01:53:28 +0100

    Quickly throw in a UI for the new settings

--- ChartPlugin/UI/ViewControllers/SettingsController.cs
+++ ChartPlugin/UI/ViewControllers/SettingsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using BeatSaberMarkupLanguage.Attributes;
 using UnityEngine;
 
@@ -19,98 +20,98 @@ namespace SongChartVisualizer.UI.ViewControllers
 		private Vector3 _noStdRot;
 
 		[UIValue("enabled-bool")]
-		public bool EnabledValue
+		internal bool EnabledValue
 		{
 			get => _configuration.EnablePlugin;
 			set => _configuration.EnablePlugin = value;
 		}
 
 		[UIValue("peak-warning-bool")]
-		public bool PeakWarningValue
+		internal bool PeakWarningValue
 		{
 			get => _configuration.PeakWarning;
 			set => _configuration.PeakWarning = value;
 		}
 
 		[UIValue("std-panel-x-pos-float")]
-		public float StdPanelXPosValue
+		internal float StdPanelXPosValue
 		{
 			get => _configuration.ChartStandardLevelPosition.x;
 			set => _stdPos = new Vector3(value, _stdPos.y, _stdPos.z);
 		}
 
 		[UIValue("std-panel-y-pos-float")]
-		public float StdPanelYPosValue
+		internal float StdPanelYPosValue
 		{
 			get => _configuration.ChartStandardLevelPosition.y;
 			set => _stdPos = new Vector3(_stdPos.x, value, _stdPos.z);
 		}
 
 		[UIValue("std-panel-z-pos-float")]
-		public float StdPanelZPosValue
+		internal float StdPanelZPosValue
 		{
 			get => _configuration.ChartStandardLevelPosition.z;
 			set => _stdPos = new Vector3(_stdPos.x, _stdPos.y, value);
 		}
 
 		[UIValue("std-panel-x-rot-float")]
-		public float StdPanelXRotValue
+		internal float StdPanelXRotValue
 		{
 			get => _configuration.ChartStandardLevelRotation.x;
 			set => _stdRot = new Vector3(value, _stdRot.y, _stdRot.z);
 		}
 
 		[UIValue("std-panel-y-rot-float")]
-		public float StdPanelYRotValue
+		internal float StdPanelYRotValue
 		{
 			get => _configuration.ChartStandardLevelRotation.y;
 			set => _stdRot = new Vector3(_stdRot.x, value, _stdRot.z);
 		}
 
 		[UIValue("std-panel-z-rot-float")]
-		public float StdPanelZRotValue
+		internal float StdPanelZRotValue
 		{
 			get => _configuration.ChartStandardLevelRotation.z;
 			set => _stdRot = new Vector3(_stdRot.x, _stdRot.y, value);
 		}
 
 		[UIValue("no-std-panel-x-pos-float")]
-		public float NoStdPanelXPosValue
+		internal float NoStdPanelXPosValue
 		{
 			get => _configuration.Chart360LevelPosition.x;
 			set => _noStdPos = new Vector3(value, _noStdPos.y, _noStdPos.z);
 		}
 
 		[UIValue("no-std-panel-y-pos-float")]
-		public float NoStdPanelYPosValue
+		internal float NoStdPanelYPosValue
 		{
 			get => _configuration.Chart360LevelPosition.y;
 			set => _noStdPos = new Vector3(_noStdPos.x, value, _noStdPos.z);
 		}
 
 		[UIValue("no-std-panel-z-pos-float")]
-		public float NoStdPanelZPosValue
+		internal float NoStdPanelZPosValue
 		{
 			get => _configuration.Chart360LevelPosition.z;
 			set => _noStdPos = new Vector3(_noStdPos.x, _noStdPos.y, value);
 		}
 
 		[UIValue("no-std-panel-x-rot-float")]
-		public float NoStdPanelXRotValue
+		internal float NoStdPanelXRotValue
 		{
 			get => _configuration.Chart360LevelRotation.x;
 			set => _noStdRot = new Vector3(value, _noStdRot.y, _noStdRot.z);
 		}
 
 		[UIValue("no-std-panel-y-rot-float")]
-		public float NoStdPanelYRotValue
+		internal float NoStdPanelYRotValue
 		{
 			get => _configuration.Chart360LevelRotation.y;
 			set => _noStdRot = new Vector3(_noStdRot.x, value, _noStdRot.z);
 		}
 
 		[UIValue("no-std-panel-z-rot-float")]
-		public float NoStdPanelZRotValue
+		internal float NoStdPanelZRotValue
 		{
 			get => _configuration.Chart360LevelRotation.z;
 			set => _noStdRot = new Vector3(_noStdRot.x, _noStdRot.y, value);
@@ -152,6 +153,81 @@ namespace SongChartVisualizer.UI.ViewControllers
 		[UIObject("no-std-rot-z-field")]
 		internal GameObject NoStdRotZField = null!;
 
+		[UIValue("show-background-bool")]
+		internal bool ShowBackground
+		{
+			get => _configuration.HasBackground;
+			set => _configuration.HasBackground = value;
+		}
+
+		[UIValue("background-opacity-float")]
+		internal float BackgroundOpacity
+		{
+			get => _configuration.BackgroundOpacity;
+			set => _configuration.BackgroundOpacity = (float) Math.Round(value, 2);
+		}
+
+		[UIAction("background-opacity-formatter")]
+		internal string BackgroundOpacityFormat(float opacity)
+		{
+			return $"{(int) (opacity * 100)}%";
+		}
+
+		[UIValue("background-color")]
+		internal Color BackgroundColor
+		{
+			get => _configuration.BackgroundColor;
+			set => _configuration.BackgroundColor = value;
+		}
+
+		[UIValue("line-color")]
+		internal Color LineColor
+		{
+			get => _configuration.LineColor;
+			set => _configuration.LineColor = value;
+		}
+
+		[UIValue("pointer-color")]
+		internal Color PointerColor
+		{
+			get => _configuration.PointerColor;
+			set => _configuration.PointerColor = value;
+		}
+
+		[UIAction("#post-parse")]
+		internal void Setup()
+		{
+			var list = new List<GameObject>
+			{
+				StdPosXField,
+				StdPosYField,
+				StdPosZField,
+				StdRotXField,
+				StdRotYField,
+				StdRotZField,
+				NoStdPosXField,
+				NoStdPosYField,
+				NoStdPosZField,
+				NoStdRotXField,
+				NoStdRotYField,
+				NoStdRotZField
+			};
+
+			foreach (var go in list)
+			{
+				ResizeValuePicker(go);
+			}
+		}
+
+		[UIAction("#apply")]
+		public void OnApply()
+		{
+			_configuration.ChartStandardLevelPosition = _stdPos;
+			_configuration.ChartStandardLevelRotation = _stdRot;
+			_configuration.Chart360LevelPosition = _noStdPos;
+			_configuration.Chart360LevelRotation = _noStdRot;
+		}
+
 		private static void ResizeValuePicker(GameObject go)
 		{
 			if (go == null)
@@ -164,32 +240,6 @@ namespace SongChartVisualizer.UI.ViewControllers
 			{
 				rectPicker.sizeDelta = new Vector2(25, rectPicker.sizeDelta.y);
 			}
-		}
-
-		[UIAction("#post-parse")]
-		internal void Setup()
-		{
-			var list = new List<GameObject>
-			{
-				StdPosXField, StdPosYField, StdPosZField, StdRotXField, StdRotYField, StdRotZField,
-				NoStdPosXField, NoStdPosYField, NoStdPosZField, NoStdRotXField, NoStdRotYField, NoStdRotZField
-			};
-
-			foreach (var go in list)
-			{
-				ResizeValuePicker(go);
-			}
-		}
-
-		[UIAction("#apply")]
-		public void OnApply()
-		{
-			_configuration.EnablePlugin = EnabledValue;
-			_configuration.PeakWarning = PeakWarningValue;
-			_configuration.ChartStandardLevelPosition = _stdPos;
-			_configuration.ChartStandardLevelRotation = _stdRot;
-			_configuration.Chart360LevelPosition = _noStdPos;
-			_configuration.Chart360LevelRotation = _noStdRot;
 		}
 	}
 }
